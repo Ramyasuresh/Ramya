e7be283a3707693add9dda20bf8689d2ffc7db5b
Ramyasuresh <31531279+Ramyasuresh@users.noreply.github.com>
2018-01-11 16:35:14 +0530

    Create 3set2.c

---
+++ 3set2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int array[i],i,n,max;
+printf("enter number of elements");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+max=array[0];
+{
+for(i=0;i<=n;i++)
+{
+if(max<array[i])
+max=array[i];
+else
+i++;
+}
+}
