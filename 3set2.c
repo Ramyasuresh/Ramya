ee2c4ecb696bd39430d66f07d6448d8a4946fab9
Ramyasuresh <31531279+Ramyasuresh@users.noreply.github.com>
2018-01-11 16:45:35 +0530

    Update 3set2.c

--- 3set2.c
+++ 3set2.c
@@ -5,13 +5,15 @@ int array[i],i,n,max;
 printf("enter number of elements");
 scanf("%d",&n);
 for(i=0;i<=n;i++)
+scanf("%d",&array[i]);
 max=array[0];
 {
 for(i=0;i<=n;i++)
 {
 if(max<array[i])
 max=array[i];
-else
 i++;
+}
+printf("max element is %d",&max);
 }
 }
